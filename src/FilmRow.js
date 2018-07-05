4a3d92ba7e94fe658e074f026d2ab558fbaaffe4
Eva Barrett <Evabarrett23@Gmail.com>
2018-07-04 22:42:05 -0700

    added getfullYear function

--- src/FilmRow.js
+++ src/FilmRow.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 
 class FilmRow extends Component {
   render() {
+    // let posterUrl = 'https://image.tmdb.org/t/p/w780/' + this.props.film.poster_path;
+    let posterUrl = `https://image.tmdb.org/t/p/w780/${this.props.film.poster_path}`;
     return (
-
+      <div className="film-row">
+        <img src={posterUrl} alt="" />
+        <div className="film-summary">
+          <h1>{this.props.film.title}</h1>
+          <p>{new Date(this.props.film.release_date).getFullYear()}</p>
+        </div>
+      </div>
     );
   }
 }
